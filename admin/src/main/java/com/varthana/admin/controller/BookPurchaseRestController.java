d18ca2b4562645c7799f44317d83dfc35395a5df
kamalgoud <kamalgoudkatta@gmail.com>
2024-02-12 10:30:25 +0530

    Updated UI and Formatted the code

--- admin/src/main/java/com/varthana/admin/controller/BookPurchaseRestController.java
+++ admin/src/main/java/com/varthana/admin/controller/BookPurchaseRestController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.*;
 import java.time.LocalDate;
 import java.util.List;
 
+@ControllerAdvice
 @RestController
 public class BookPurchaseRestController {
 
